3ec0b0c8588ea634d9e9a1bfe7accbad382f5e53
rohithngani <rohithngani@gmail.com>
2022-05-05 19:14:24 +0530

    Project

---
+++ src/test/java/com/scripts/PQRTestCase2.java
@@ -0,0 +1,12 @@
+package com.scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PQRTestCase2 {
+
+	@Test
+	public void tc2() {
+		Reporter.log("Hello selenium", true);
+	}
+}
---
+++ src/test/java/com/scripts/PQRTestCase3.java
@@ -0,0 +1,12 @@
+package com.scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PQRTestCase3 {
+
+	@Test
+	public void tc3() {
+		Reporter.log("Hello SQL", true);
+	}
+}
